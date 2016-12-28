d01ccd11b95a434687ea678eaa9517c84e10c34d
Artem Tabalin <tabalinas@gmail.com>
2016-12-28 17:33:31 +0100

    App: Handle properties binding

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,25 @@
+
 const ProductList = React.createClass({
+    
     render() {
+        const products = Data.map(product => {
+            return (
+                <Product 
+                    key={'product-' + product.id}
+                    id={product.id}
+                    title={product.title}
+                    description={product.description}
+                    url={product.url}
+                    votes={product.votes}
+                    submitter_avatar_url={product.submitter_avatar_url}
+                    product_image_url={product.product_image_url}      
+                />
+            )
+        });
+
         return (
             <div className='product-list'>
-                <Product />
+                {products}
             </div>
         )
     }
@@ -10,21 +27,29 @@ const ProductList = React.createClass({
 
 const Product = React.createClass({
     render() {
-        return <div className='product-item'>
-            <div className='image'>
-                <img src='imgs/product-image.png' />
-            </div>
-            <div className='middle aligned content'>
-                <div className='description'>
-                    <a>Fort Knight</a>
-                    <p>Authentic renaissance actors, delivered in just two weeks.</p>
+        return (
+            <div className='product-item'>
+                <div className='image'>
+                    <img src='{this.props.product_image_url}' />
                 </div>
-                <div className='extra'>
-                    <span>Submitted by:</span>
-                    <img src='imgs/avatar.jpg' />
+                <div className='middle aligned content'>
+                    <div className='header'>
+                        <a>
+                            <i className='large caret up icon'></i>
+                        </a>
+                        {this.props.votes}
+                    </div>
+                    <div className='description'>
+                        <a href='{this.props.url}'>{this.props.title}</a>
+                        <p>{this.props.description}</p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img src='{this.props.submitter_avatar_url}' />
+                    </div>
                 </div>
             </div>
-        </div>
+        )
     }
 })
 
---
+++ public/js/data.js
@@ -0,0 +1,9 @@
+var Data = [{
+    id: 1,
+    title: 'Yellow Pail',
+    description: 'On-demand sand castle construction expertise.',
+    url: '#',
+    votes: 10,
+    submitter_avatar_url: 'images/avatar.jpg',
+    product_image_url: 'images/product-image.png',
+}];
